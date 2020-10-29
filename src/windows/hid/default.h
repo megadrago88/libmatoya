a70832077d7a8a7d45104bf1dbb5c183a29d5f45
Christopher Dickson <chrisd1100@gmail.com>
2020-10-29 09:10:55 -0400

    default hid recursion fix

--- src/windows/hid/default.h
+++ src/windows/hid/default.h
@@ -23,57 +23,62 @@ static void hid_default_map_values(MTY_Controller *c)
 		c->numValues = MTY_CVALUE_DPAD + 1;
 
 	// Swap positions
-	for (uint8_t x = 0; x < c->numValues; x++) {
-		MTY_Value *v = &c->values[x];
+	for (uint8_t i = 0; i < 3; i++) {
+		bool swapped = false;
 
-		retry:
+		for (uint8_t x = 0; x < c->numValues; x++) {
+			MTY_Value *v = &c->values[x];
 
-		switch (v->usage) {
-			case 0x30: // X -> Left Stick X
-				if (x != MTY_CVALUE_THUMB_LX) {
-					hid_default_swap_value(c->values, MTY_CVALUE_THUMB_LX, v);
-					goto retry;
-				}
-				break;
-			case 0x31: // Y -> Left Stick Y
-				if (x != MTY_CVALUE_THUMB_LY) {
-					hid_default_swap_value(c->values, MTY_CVALUE_THUMB_LY, v);
-					goto retry;
-				}
-				break;
-			case 0x32: // Z -> Right Stick X
-				if (x != MTY_CVALUE_THUMB_RX) {
-					hid_default_swap_value(c->values, MTY_CVALUE_THUMB_RX, v);
-					goto retry;
-				}
-				break;
-			case 0x35: // Rz -> Right Stick Y
-				if (x != MTY_CVALUE_THUMB_RY) {
-					hid_default_swap_value(c->values, MTY_CVALUE_THUMB_RY, v);
-					goto retry;
-				}
-				break;
-			case 0x33: // Rx -> Left Trigger
-				have_lt = true;
-				if (x != MTY_CVALUE_TRIGGER_L) {
-					hid_default_swap_value(c->values, MTY_CVALUE_TRIGGER_L, v);
-					goto retry;
-				}
-				break;
-			case 0x34: // Ry -> Right Trigger
-				have_rt = true;
-				if (x != MTY_CVALUE_TRIGGER_R) {
-					hid_default_swap_value(c->values, MTY_CVALUE_TRIGGER_R, v);
-					goto retry;
-				}
-				break;
-			case 0x39: // Hat -> DPAD
-				if (x != MTY_CVALUE_DPAD) {
-					hid_default_swap_value(c->values, MTY_CVALUE_DPAD, v);
-					goto retry;
-				}
-				break;
+			switch (v->usage) {
+				case 0x30: // X -> Left Stick X
+					if (x != MTY_CVALUE_THUMB_LX) {
+						hid_default_swap_value(c->values, MTY_CVALUE_THUMB_LX, v);
+						swapped = true;
+					}
+					break;
+				case 0x31: // Y -> Left Stick Y
+					if (x != MTY_CVALUE_THUMB_LY) {
+						hid_default_swap_value(c->values, MTY_CVALUE_THUMB_LY, v);
+						swapped = true;
+					}
+					break;
+				case 0x32: // Z -> Right Stick X
+					if (x != MTY_CVALUE_THUMB_RX) {
+						hid_default_swap_value(c->values, MTY_CVALUE_THUMB_RX, v);
+						swapped = true;
+					}
+					break;
+				case 0x35: // Rz -> Right Stick Y
+					if (x != MTY_CVALUE_THUMB_RY) {
+						hid_default_swap_value(c->values, MTY_CVALUE_THUMB_RY, v);
+						swapped = true;
+					}
+					break;
+				case 0x33: // Rx -> Left Trigger
+					have_lt = true;
+					if (x != MTY_CVALUE_TRIGGER_L) {
+						hid_default_swap_value(c->values, MTY_CVALUE_TRIGGER_L, v);
+						swapped = true;
+					}
+					break;
+				case 0x34: // Ry -> Right Trigger
+					have_rt = true;
+					if (x != MTY_CVALUE_TRIGGER_R) {
+						hid_default_swap_value(c->values, MTY_CVALUE_TRIGGER_R, v);
+						swapped = true;
+					}
+					break;
+				case 0x39: // Hat -> DPAD
+					if (x != MTY_CVALUE_DPAD) {
+						hid_default_swap_value(c->values, MTY_CVALUE_DPAD, v);
+						swapped = true;
+					}
+					break;
+			}
 		}
+
+		if (!swapped)
+			break;
 	}
 
 	// Convert to int16_t
@@ -160,7 +165,7 @@ static void hid_default_state(struct hid *hid, void *data, ULONG dsize, MTY_Msg 
 	}
 
 	// Values
-	for (uint32_t x = 0; x < hid->caps.NumberInputValueCaps && x < MTY_CVALUE_MAX; x++) {
+	for (uint32_t x = 0; x < hid->caps.NumberInputValueCaps && c->numValues < MTY_CVALUE_MAX; x++) {
 		const HIDP_VALUE_CAPS *vcap = &hid->vcaps[x];
 
 		ULONG value = 0;
@@ -171,12 +176,12 @@ static void hid_default_state(struct hid *hid, void *data, ULONG dsize, MTY_Msg 
 		}
 
 		if (e == HIDP_STATUS_SUCCESS) {
-			c->values[x].usage = vcap->Range.UsageMin;
-			c->values[x].data = (int16_t) value;
-			c->values[x].min = (int16_t) vcap->LogicalMin;
-			c->values[x].max = (int16_t) vcap->LogicalMax;
+			MTY_Value *v = &c->values[c->numValues++];
 
-			c->numValues++;
+			v->usage = vcap->Range.UsageMin;
+			v->data = (int16_t) value;
+			v->min = (int16_t) vcap->LogicalMin;
+			v->max = (int16_t) vcap->LogicalMax;
 		}
 	}
 
