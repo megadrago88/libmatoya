40b00206d4620d0b416cce9873d007808590f1ec
Christopher Dickson <chrisd1100@gmail.com>
2020-11-17 15:52:46 -0500

    ps5 output

--- src/hid/driver.h
+++ src/hid/driver.h
@@ -324,6 +324,9 @@ static void hid_driver_rumble(struct hid *hid, uint32_t id, uint16_t low, uint16
 		case MTY_HID_DRIVER_PS4:
 			hid_ps4_rumble(device, low, high);
 			break;
+		case MTY_HID_DRIVER_PS5:
+			hid_ps5_rumble(device, low, high);
+			break;
 		case MTY_HID_DRIVER_XBOX:
 			hid_xbox_rumble(device, low, high);
 			break;
