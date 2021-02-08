dbfd71db4ee7f06298e4809e09ded46a176b24d4
Christopher Dickson <chrisd1100@gmail.com>
2021-02-08 12:58:40 -0500

    audio cleanup

--- src/unix/linux/android/audio.c
+++ src/unix/linux/android/audio.c
@@ -7,6 +7,7 @@
 #include "matoya.h"
 
 #include <string.h>
+#include <stdio.h>
 #include <math.h>
 
 #include <aaudio/AAudio.h>
@@ -22,8 +23,8 @@ struct MTY_Audio {
 	uint32_t sample_rate;
 	uint32_t min_buffer;
 	uint32_t max_buffer;
-	bool cb_stopped;
 	bool flushing;
+	bool playing;
 
 	uint8_t *buffer;
 	size_t size;
@@ -38,54 +39,36 @@ static aaudio_data_callback_result_t audio_callback(AAudioStream *stream, void *
 	void *audioData, int32_t numFrames)
 {
 	MTY_Audio *ctx = userData;
-	bool stop = false;
 
 	MTY_MutexLock(ctx->mutex);
 
 	size_t want_size = numFrames * AUDIO_CHANNELS * 2;
 
-	if (ctx->size >= want_size) {
+	if (ctx->playing && ctx->size >= want_size) {
 		memcpy(audioData, ctx->buffer, want_size);
 		ctx->size -= want_size;
 
 		memmove(ctx->buffer, ctx->buffer + want_size, ctx->size);
 
 	} else {
-		// Underrun
-		ctx->size = 0;
-		stop = true;
+		memset(audioData, 0, want_size);
 	}
 
 	MTY_MutexUnlock(ctx->mutex);
 
-	return stop ? AAUDIO_CALLBACK_RESULT_STOP : AAUDIO_CALLBACK_RESULT_CONTINUE;
+	return AAUDIO_CALLBACK_RESULT_CONTINUE;
 }
 
 MTY_Audio *MTY_AudioCreate(uint32_t sampleRate, uint32_t minBuffer, uint32_t maxBuffer)
 {
 	MTY_Audio *ctx = MTY_Alloc(1, sizeof(MTY_Audio));
-
 	ctx->sample_rate = sampleRate;
 	ctx->mutex = MTY_MutexCreate();
 	ctx->buffer = MTY_Alloc(AUDIO_BUF_SIZE, 1);
-	ctx->cb_stopped = true;
 
 	uint32_t frames_per_ms = lrint((float) sampleRate / 1000.0f);
 	ctx->min_buffer = minBuffer * frames_per_ms * AUDIO_CHANNELS * 2;
 	ctx->max_buffer = maxBuffer * frames_per_ms * AUDIO_CHANNELS * 2;
-
-	AAudio_createStreamBuilder(&ctx->builder);
-	AAudioStreamBuilder_setDeviceId(ctx->builder, AAUDIO_UNSPECIFIED);
-	AAudioStreamBuilder_setSampleRate(ctx->builder, sampleRate);
-	AAudioStreamBuilder_setChannelCount(ctx->builder, AUDIO_CHANNELS);
-	AAudioStreamBuilder_setFormat(ctx->builder, AAUDIO_FORMAT_PCM_I16);
-	AAudioStreamBuilder_setPerformanceMode(ctx->builder, AAUDIO_PERFORMANCE_MODE_LOW_LATENCY);
-	AAudioStreamBuilder_setErrorCallback(ctx->builder, audio_error, ctx);
-	AAudioStreamBuilder_setDataCallback(ctx->builder, audio_callback, ctx);
-	AAudioStreamBuilder_setBufferCapacityInFrames(ctx->builder, sampleRate); // 1s
-
-	AAudioStreamBuilder_openStream(ctx->builder, &ctx->stream);
-	AAudioStream_setBufferSizeInFrames(ctx->stream, 0);
 
 	return ctx;
 }
@@ -97,11 +80,50 @@ uint32_t MTY_AudioGetQueuedMs(MTY_Audio *ctx)
 
 void MTY_AudioStop(MTY_Audio *ctx)
 {
+	if (ctx->stream) {
+		MTY_MutexLock(ctx->mutex);
+
+		ctx->playing = false;
+		ctx->flushing = false;
+		ctx->size = 0;
+
+		MTY_MutexUnlock(ctx->mutex);
+
+		AAudioStream_requestStop(ctx->stream);
+		AAudioStream_close(ctx->stream);
+		ctx->stream = NULL;
+	}
+
+	if (ctx->builder) {
+		AAudioStreamBuilder_delete(ctx->builder);
+		ctx->builder = NULL;
+	}
+}
+
+static void audio_start(MTY_Audio *ctx)
+{
+	if (!ctx->builder) {
+		AAudio_createStreamBuilder(&ctx->builder);
+		AAudioStreamBuilder_setDeviceId(ctx->builder, AAUDIO_UNSPECIFIED);
+		AAudioStreamBuilder_setSampleRate(ctx->builder, ctx->sample_rate);
+		AAudioStreamBuilder_setChannelCount(ctx->builder, AUDIO_CHANNELS);
+		AAudioStreamBuilder_setFormat(ctx->builder, AAUDIO_FORMAT_PCM_I16);
+		AAudioStreamBuilder_setPerformanceMode(ctx->builder, AAUDIO_PERFORMANCE_MODE_LOW_LATENCY);
+		AAudioStreamBuilder_setErrorCallback(ctx->builder, audio_error, ctx);
+		AAudioStreamBuilder_setDataCallback(ctx->builder, audio_callback, ctx);
+	}
+
+	if (!ctx->stream) {
+		AAudioStreamBuilder_openStream(ctx->builder, &ctx->stream);
+		AAudioStream_requestStart(ctx->stream);
+	}
 }
 
 void MTY_AudioQueue(MTY_Audio *ctx, const int16_t *frames, uint32_t count)
 {
 	size_t data_size = count * AUDIO_CHANNELS * 2;
+
+	audio_start(ctx);
 
 	MTY_MutexLock(ctx->mutex);
 
@@ -109,22 +131,17 @@ void MTY_AudioQueue(MTY_Audio *ctx, const int16_t *frames, uint32_t count)
 		ctx->flushing = true;
 
 	if (ctx->size == 0) {
-		if (!ctx->cb_stopped)
-			AAudioStream_requestStop(ctx->stream);
-
-		ctx->cb_stopped = true;
+		ctx->playing = false;
 		ctx->flushing = false;
 	}
 
-	if (!ctx->flushing && data_size + ctx->size < AUDIO_BUF_SIZE) {
+	if (!ctx->flushing && data_size + ctx->size <= AUDIO_BUF_SIZE) {
 		memcpy(ctx->buffer + ctx->size, frames, data_size);
 		ctx->size += data_size;
 	}
 
-	if (ctx->cb_stopped && ctx->size >= ctx->min_buffer) {
-		AAudioStream_requestStart(ctx->stream);
-		ctx->cb_stopped = false;
-	}
+	if (ctx->size >= ctx->min_buffer)
+		ctx->playing = true;
 
 	MTY_MutexUnlock(ctx->mutex);
 }
@@ -136,13 +153,7 @@ void MTY_AudioDestroy(MTY_Audio **audio)
 
 	MTY_Audio *ctx = *audio;
 
-	if (ctx->stream) {
-		AAudioStream_requestStop(ctx->stream);
-		AAudioStream_close(ctx->stream);
-	}
-
-	if (ctx->builder)
-		AAudioStreamBuilder_delete(ctx->builder);
+	MTY_AudioStop(ctx);
 
 	MTY_MutexDestroy(&ctx->mutex);
 	MTY_Free(ctx->buffer);
