4ba6b2995fa1f92fa13db491778d72b535d50e1a
Christopher Dickson <chrisd1100@gmail.com>
2021-02-22 21:24:02 -0500

    aes gcm performance improvments

--- src/unix/linux/android/aes-gcm.c
+++ src/unix/linux/android/aes-gcm.c
@@ -11,19 +11,25 @@
 
 #include "jnih.h"
 
+#define AES_GCM_ENCRYPT 0x00000001
+#define AES_GCM_DECRYPT 0x00000002
+
+#define AES_GCM_NUM_BUFS 6
+#define AES_GCM_MAX      (8 * 1024)
+
 struct MTY_AESGCM {
 	jobject gcm;
 	jobject key;
 
-	uint8_t *ebuf[2];
-	size_t esize;
+	jclass cls_gps;
+	jclass cls_cipher;
 
-	uint8_t *dbuf[2];
-	size_t dsize;
+	jmethodID m_gps_constructor;
+	jmethodID m_cipher_init;
+	jmethodID m_cipher_do_final;
+
+	jbyteArray buf[AES_GCM_NUM_BUFS];
 };
-
-#define AES_GCM_ENCRYPT 0x00000001
-#define AES_GCM_DECRYPT 0x00000002
 
 MTY_AESGCM *MTY_AESGCMCreate(const void *key)
 {
@@ -31,15 +37,37 @@ MTY_AESGCM *MTY_AESGCMCreate(const void *key)
 
 	JNIEnv *env = MTY_JNIEnv();
 
+	// The cipher handle
 	jstring jalg = jnih_strdup(env, "AES/GCM/NoPadding");
-	ctx->gcm = jnih_static_obj(env, "javax/crypto/Cipher", "getInstance", "(Ljava/lang/String;)Ljavax/crypto/Cipher;", jalg);
+	ctx->gcm = jnih_static_obj(env, "javax/crypto/Cipher", "getInstance",
+		"(Ljava/lang/String;)Ljavax/crypto/Cipher;", jalg);
+	jnih_retain(env, &ctx->gcm);
 
+	// 128 bit AES key
 	jbyteArray jkey = jnih_dup(env, key, 16);
 	jstring jalg_key = jnih_strdup(env, "AES");
 	ctx->key = jnih_new(env, "javax/crypto/spec/SecretKeySpec", "([BLjava/lang/String;)V", jkey, jalg_key);
-
-	jnih_retain(env, &ctx->gcm);
 	jnih_retain(env, &ctx->key);
+
+	// Preload classes for performance, these need global references so the method IDs are valid
+	ctx->cls_gps = (*env)->FindClass(env, "javax/crypto/spec/GCMParameterSpec");
+	jnih_retain(env, &ctx->cls_gps);
+
+	ctx->cls_cipher = (*env)->FindClass(env, "javax/crypto/Cipher");
+	jnih_retain(env, &ctx->cls_cipher);
+
+	// Preload methods for performance
+	ctx->m_gps_constructor = (*env)->GetMethodID(env, ctx->cls_gps, "<init>", "(I[BII)V");
+	ctx->m_cipher_init = (*env)->GetMethodID(env, ctx->cls_cipher, "init",
+		"(ILjava/security/Key;Ljava/security/spec/AlgorithmParameterSpec;)V");
+
+	ctx->m_cipher_do_final = (*env)->GetMethodID(env, ctx->cls_cipher, "doFinal", "([BII[B)I");
+
+	// Preallocate byte buffers
+	for (uint8_t x = 0; x < AES_GCM_NUM_BUFS; x++) {
+		ctx->buf[x] = (*env)->NewByteArray(env, AES_GCM_MAX);
+		jnih_retain(env, &ctx->buf[x]);
+	}
 
 	jnih_free(env, jalg_key);
 	jnih_free(env, jkey);
@@ -52,48 +80,27 @@ bool MTY_AESGCMEncrypt(MTY_AESGCM *ctx, const void *nonce, const void *plainText
 	void *hash, void *cipherText)
 {
 	bool r = true;
+
 	JNIEnv *env = MTY_JNIEnv();
 
-	if (size + 16 > ctx->esize) {
-		ctx->esize = size + 16;
-		ctx->ebuf[0] = MTY_Realloc(ctx->ebuf[0], size, 1);
-		ctx->ebuf[1] = MTY_Realloc(ctx->ebuf[1], ctx->esize, 1);
-	}
+	(*env)->SetByteArrayRegion(env, ctx->buf[0], 0, 12, nonce);
+	(*env)->SetByteArrayRegion(env, ctx->buf[1], 0, size, plainText);
 
-	memcpy(ctx->ebuf[0], plainText, size);
+	jobject spec = (*env)->NewObject(env, ctx->cls_gps, ctx->m_gps_constructor, 128, ctx->buf[0], 0, 12);
 
-	jobject in = jnih_wrap(env, ctx->ebuf[0], size);
-	jobject out = jnih_wrap(env, ctx->ebuf[1], size + 16);
-
-	jbyteArray jnonce = jnih_dup(env, nonce, 12);
-	jobject spec = jnih_new(env, "javax/crypto/spec/GCMParameterSpec", "(I[B)V", 128, jnonce);
+	(*env)->CallVoidMethod(env, ctx->gcm, ctx->m_cipher_init, AES_GCM_ENCRYPT, ctx->key, spec);
+	(*env)->CallIntMethod(env, ctx->gcm, ctx->m_cipher_do_final, ctx->buf[1], 0, size, ctx->buf[2]);
 	if (jnih_catch(env)) {
 		r = false;
 		goto except;
 	}
 
-	jnih_void(env, ctx->gcm, "init", "(ILjava/security/Key;Ljava/security/spec/AlgorithmParameterSpec;)V",
-		AES_GCM_ENCRYPT, ctx->key, spec);
-	if (jnih_catch(env)) {
-		r = false;
-		goto except;
-	}
-
-	jnih_int(env, ctx->gcm, "doFinal", "(Ljava/nio/ByteBuffer;Ljava/nio/ByteBuffer;)I", in, out);
-	if (jnih_catch(env)) {
-		r = false;
-		goto except;
-	}
-
-	memcpy(cipherText, ctx->ebuf[1], size);
-	memcpy(hash, ctx->ebuf[1] + size, 16);
+	(*env)->GetByteArrayRegion(env, ctx->buf[2], 0, size, cipherText);
+	(*env)->GetByteArrayRegion(env, ctx->buf[2], size, 16, hash);
 
 	except:
 
 	jnih_free(env, spec);
-	jnih_free(env, jnonce);
-	jnih_free(env, out);
-	jnih_free(env, in);
 
 	return r;
 }
@@ -102,48 +109,27 @@ bool MTY_AESGCMDecrypt(MTY_AESGCM *ctx, const void *nonce, const void *cipherTex
 	const void *hash, void *plainText)
 {
 	bool r = true;
+
 	JNIEnv *env = MTY_JNIEnv();
 
-	if (size + 16 > ctx->dsize) {
-		ctx->dsize = size + 16;
-		ctx->dbuf[0] = MTY_Realloc(ctx->dbuf[0], ctx->dsize, 1);
-		ctx->dbuf[1] = MTY_Realloc(ctx->dbuf[1], size, 1);
-	}
+	(*env)->SetByteArrayRegion(env, ctx->buf[3], 0, 12, nonce);
+	(*env)->SetByteArrayRegion(env, ctx->buf[4], 0, size, cipherText);
+	(*env)->SetByteArrayRegion(env, ctx->buf[4], size, 16, hash);
 
-	memcpy(ctx->dbuf[0], cipherText, size);
-	memcpy(ctx->dbuf[0] + size, hash, 16);
+	jobject spec = (*env)->NewObject(env, ctx->cls_gps, ctx->m_gps_constructor, 128, ctx->buf[3], 0, 12);
 
-	jobject in = jnih_wrap(env, ctx->dbuf[0], size + 16);
-	jobject out = jnih_wrap(env, ctx->dbuf[1], size);
-
-	jbyteArray jnonce = jnih_dup(env, nonce, 12);
-	jobject spec = jnih_new(env, "javax/crypto/spec/GCMParameterSpec", "(I[B)V", 128, jnonce);
+	(*env)->CallVoidMethod(env, ctx->gcm, ctx->m_cipher_init, AES_GCM_DECRYPT, ctx->key, spec);
+	(*env)->CallIntMethod(env, ctx->gcm, ctx->m_cipher_do_final, ctx->buf[4], 0, size + 16, ctx->buf[5]);
 	if (jnih_catch(env)) {
 		r = false;
 		goto except;
 	}
 
-	jnih_void(env, ctx->gcm, "init", "(ILjava/security/Key;Ljava/security/spec/AlgorithmParameterSpec;)V",
-		AES_GCM_DECRYPT, ctx->key, spec);
-	if (jnih_catch(env)) {
-		r = false;
-		goto except;
-	}
-
-	jnih_int(env, ctx->gcm, "doFinal", "(Ljava/nio/ByteBuffer;Ljava/nio/ByteBuffer;)I", in, out);
-	if (jnih_catch(env)) {
-		r = false;
-		goto except;
-	}
-
-	memcpy(plainText, ctx->dbuf[1], size);
+	(*env)->GetByteArrayRegion(env, ctx->buf[5], 0, size, plainText);
 
 	except:
 
 	jnih_free(env, spec);
-	jnih_free(env, jnonce);
-	jnih_free(env, out);
-	jnih_free(env, in);
 
 	return r;
 }
@@ -157,11 +143,11 @@ void MTY_AESGCMDestroy(MTY_AESGCM **aesgcm)
 
 	JNIEnv *env = MTY_JNIEnv();
 
-	for (uint8_t x = 0; x < 2; x++) {
-		MTY_Free(ctx->ebuf[x]);
-		MTY_Free(ctx->dbuf[x]);
-	}
+	for (uint8_t x = 0; x < AES_GCM_NUM_BUFS; x++)
+		jnih_release(env, &ctx->buf[x]);
 
+	jnih_release(env, &ctx->cls_gps);
+	jnih_release(env, &ctx->cls_cipher);
 	jnih_release(env, &ctx->key);
 	jnih_release(env, &ctx->gcm);
 
